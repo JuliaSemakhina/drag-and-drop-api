19074f952dcf6cbd96e3f9b0a77fa3a16f2cdd97
Julia Semakhina <contremoi@yandex.ru>
2022-05-27 19:06:55 +0300

    final files

--- script.js
+++ script.js
@@ -1,70 +1,28 @@
-document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
-    const dropZoneElement = inputElement.closest('.drop-zone');
+const draggableElement = document.querySelector('#myDraggableElement');
 
-    dropZoneElement.addEventListener('click', (e) => {
-        inputElement.click();
-    })
+draggableElement.addEventListener("dragstart", (e)=>{
+    e.dataTransfer.setData("text/plain", draggableElement.id);
 
-    inputElement.addEventListener('change', (e) => {
-        if (inputElement.files.length) {
-            updateThumbnail(dropZoneElement, inputElement.files[0]);
-        }
-    });
-
-
-    dropZoneElement.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        dropZoneElement.classList.add("drop-zone--over");
-    });
-
-    ["dragleave", "dragend"].forEach(type => {
-        dropZoneElement.addEventListener(type, e => {
-            dropZoneElement.classList.remove("drop-zone--over");
+    for(const dropZone of document.querySelectorAll('.drop-zone')){
+        dropZone.addEventListener('dragover', (e)=>{
+            e.preventDefault();
+            dropZone.classList.add('drop-zone--over');
         });
-    });
 
-    dropZoneElement.addEventListener("drop", (e) => {
-        e.preventDefault();
 
-        if (e.dataTransfer.files.length) {
-            inputElement.files = e.dataTransfer.files;
-            updateThumbnail(dropZoneElement, e.dataTransfer.files[0])
-        }
+        dropZone.addEventListener('dragleave', (e)=>{
+            e.preventDefault();
+            dropZone.classList.remove('drop-zone--over');
+        })
 
-        dropZoneElement.classList.remove("drop-zone--over");
-    });
-});
+        dropZone.addEventListener('drop', (e)=>{
+            e.preventDefault();
 
-// //Updates the thumbnail on a drop zone element
-// @param {HTMLElement} dropZoneElement
-// @param {File} file
+            const droppedElementId = e.dataTransfer.getData("text/plain");
+            const droppedElement = document.getElementById(droppedElementId);
+            dropZone.appendChild(droppedElement);
 
-function updateThumbnail(dropZoneElement, file) {
-    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-    //First time - remove the prompt
-    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-        dropZoneElement.querySelector(".drop-zone__prompt").remove();
+            dropZone.classList.remove('drop-zone--over');
+        });
     }
-
-    //Let's create the thumbnail element
-    if (!thumbnailElement) {
-        thumbnailElement = document.createElement("div");
-        thumbnailElement.classList.add("drop-zone__thumb");
-        dropZoneElement.appendChild(thumbnailElement);
-    }
-
-    thumbnailElement.dataset.label = file.name;
-
-    //Show thumbnail for image
-    if (file.type.startsWith("image/")) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-        }
-    } else {
-        thumbnailElement.style.backgroundImage - null;
-    }
-
-}
+})
